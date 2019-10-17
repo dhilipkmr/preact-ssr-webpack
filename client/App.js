b52c9e6fa9abb504b348b019bc609aefae9604f9
dhilipkumarV <dhilip.velusamy@go-mmt.com>
2019-10-17 16:06:19 +0530

    master

--- client/App.js
+++ client/App.js
@@ -1,11 +1,11 @@
 import { h, Component } from 'preact';
-import Home from './Container/Home'
+// import Home from './Container/Home'
 
 export default class App extends Component {
 	render() {
 		return (
 			<div id="app">
-				<Home path="/" />
+				<div>HI</div>
 			</div>
 		);
 	}
